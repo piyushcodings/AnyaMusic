6e3f3db5df698255b1fe4c10e97940d52b5d3b06
piyushcodings <118120770+piyushcodings@users.noreply.github.com>
2026-03-25 12:06:14 +0530

    Update button URLs in buttons.py

--- IstkharMusic/utils/stuffs/buttons.py
+++ IstkharMusic/utils/stuffs/buttons.py
@@ -5,8 +5,8 @@ import config
 class BUTTONS(object):
     ABUTTON = [
     [
-        InlineKeyboardButton("sυᴘᴘᴏʀᴛ", url="https://t.me/THUNDERDEVS"),
-        InlineKeyboardButton("υᴘᴅᴧᴛᴇs", url="https://t.me/IamIstkhar")
+        InlineKeyboardButton("sυᴘᴘᴏʀᴛ", url="https://t.me/+6mT2_lvTzVQwODU1"),
+        InlineKeyboardButton("υᴘᴅᴧᴛᴇs", url="https://t.me/PiyushxCodes")
     ],
     [
         InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=config.OWNER_ID),
